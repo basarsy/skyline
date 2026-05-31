1f31260cbbdb0ebb6ba546a2091c810c3b8a27f5
basarsy <basarsoyturk03@gmail.com>
2026-05-31 17:18:18 +0100

    refactor: harden Kafka security and improve topic scalability

--- src/main/java/com/basarsy/skyline/common/notification/config/KafkaTopicConfig.java
+++ src/main/java/com/basarsy/skyline/common/notification/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.basarsy.skyline.common.notification.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -10,11 +11,17 @@ public class KafkaTopicConfig {
 
     public static final String FLIGHT_CANCELLED_TOPIC = "skyline.flights.cancellation";
 
+    @Value("${spring.kafka.topic.flight-cancellation.partitions:3}")
+    private int partitions;
+
+    @Value("${spring.kafka.topic.flight-cancellation.replicas:1}")
+    private int replicas;
+
     @Bean
     public NewTopic flightCancelledTopic() {
         return TopicBuilder.name(FLIGHT_CANCELLED_TOPIC)
-                .partitions(3)
-                .replicas(1)
+                .partitions(partitions)
+                .replicas(replicas)
                 .build();
     }
 }
